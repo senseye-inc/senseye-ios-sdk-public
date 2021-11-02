eddb7daee712e3ef6f42fba3ec0b62784582e0fc
dkman94 <deepak.s.kumar.85@gmail.com>
2021-11-02 12:53:48 -0700

    initial commit, proj structure

--- Sources/senseye-ios-sdk/senseye_ios_sdk.swift
+++ Sources/senseye-ios-sdk/senseye_ios_sdk.swift
@@ -1,3 +1,16 @@
 struct senseye_ios_sdk {
     var text = "Hello, World!"
 }
+
+public class SenseyeSDK {
+    
+    private var result = "Initial result!"
+    
+    public init() {
+        result = "Post-init result!"
+    }
+    
+    public func retreiveResult() -> String {
+        return result
+    }
+}

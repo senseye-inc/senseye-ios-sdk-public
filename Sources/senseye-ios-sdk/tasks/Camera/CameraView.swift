a8c8f0ac7170c055e1242c5f5bc548a67a113d6a
Frank Oftring <frank.oftring@senseye.co>
2022-07-15 11:40:26 -0400

    change sheet presentation to overlay

--- Sources/senseye-ios-sdk/tasks/Camera/CameraView.swift
+++ Sources/senseye-ios-sdk/tasks/Camera/CameraView.swift
@@ -14,57 +14,55 @@ struct CameraView: View {
     
     @EnvironmentObject var cameraService: CameraService
     @EnvironmentObject var tabController: TabController
-
-    var body: some View {
-        ZStack {
-            CameraPreview(cameraService: cameraService)
-            VStack {
-                Button { } label: {
-                    CameraButtonOverlayView()
-                        .onTapGesture(count: 2) {
-                            tabController.proceedToNextTab()
-                        }
-                }
-                .disabled(!cameraService.shouldSetupCaptureSession)
-            }
-        }
-        .onAppear {
-            cameraService.start()
-            Log.info("displayed cameraview")
-        }
-        .edgesIgnoringSafeArea(.all)
-        .alert("Need Camera Access", isPresented: $cameraService.shouldShowCameraPermissionsDeniedAlert) {
-            Button("Go to settings") {
-                cameraService.goToSettings()
-            }
-        } message: {
-            Text("Change camera permissions in your settings.")
-        }
-        .sheet(isPresented: $cameraService.shouldDisplayPretaskTutorial, onDismiss: {
-            dismissAllSheets()
-        }, content: {
-            let taskInfo = tabController.taskInfoForNextTab()
-            PreTaskInstructionView(title: taskInfo.0, description: taskInfo.1)
-        })
-
-    }
+    @State private var showingOverlay = false
     
-    func dismissAllSheets() {
-        Log.info("dismissed the sheet")
-        cameraService.shouldDisplayPretaskTutorial = false
+    var body: some View {
+        GeometryReader { _ in
+            ZStack {
+                CameraPreview(cameraService: cameraService)
+                VStack {
+                    Button { } label: {
+                        CameraButtonOverlayView()
+                            .onTapGesture(count: 2) {
+                                tabController.proceedToNextTab()
+                            }
+                    }
+                    .disabled(!cameraService.shouldSetupCaptureSession || showingOverlay)
+                }
+                
+                if showingOverlay {
+                    let taskInfo = tabController.taskInfoForNextTab()
+                    SenseyeInfoOverlay(title: taskInfo.0, description: taskInfo.1, showingOverlay: $showingOverlay)
+                }
+            }
+            .onAppear {
+                cameraService.start()
+                showingOverlay.toggle()
+                Log.info("displayed cameraview")
+            }
+            .edgesIgnoringSafeArea(.all)
+            .alert("Need Camera Access", isPresented: $cameraService.shouldShowCameraPermissionsDeniedAlert) {
+                Button("Go to settings") {
+                    cameraService.goToSettings()
+                }
+            } message: {
+                Text("Change camera permissions in your settings.")
+            }
+        }
+        .animation(.easeIn(duration: 0.5))
     }
 }
 
 @available(iOS 13.0, *)
 struct CameraPreview: UIViewRepresentable {
-
+    
     let cameraService: CameraService
-
+    
     func makeUIView(context: Context) -> UIView {
         let view = UIView(frame: UIScreen.main.bounds)
         cameraService.setupVideoPreviewLayer(for: view)
         return view
     }
-
+    
     func updateUIView(_ uiView: UIView, context: Context) { }
 }

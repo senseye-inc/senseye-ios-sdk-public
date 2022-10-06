7fc96c7d1373d284e979f8dd3898e1ffb16ebaf3
BobbyS <44246074+bobbys-dev@users.noreply.github.com>
2022-10-06 15:15:38 -0500

    Bobbysrisan/sc 23813/ble bluetooth crash on pulseox parse (#81)
    * Parameterize and abstract values into constants. Add bit manipulation helpers
    
    * Return empty array if no header bytes in BLE data

--- Sources/senseye-ios-sdk/Utilities/BerryMedPulseOx.swift
+++ Sources/senseye-ios-sdk/Utilities/BerryMedPulseOx.swift
@@ -24,7 +24,26 @@ struct HeartParams: Codable {
 
 struct BerryMedPulseOx: HeartParamsParsingProtocol {
     // TODO: barGraph, signalStrength, noSignal, probeUnplugged, pulseBeep, noFinger, pulseResearch
-    let berrymedDataPackSize: Int = 5
+
+    private struct Constants {
+        static let berrymedDataPackSize: Int = 5
+        static let bitIndexOfHeader: UInt8 = 7
+
+        static let byteIndexOfPlethysmographBits = 1
+        static let byteIndexOfPulseRateBits7 = 2
+        static let byteIndexOfPulseRateBits0_6 = 3
+        static let byteIndexOfSpo2Bits = 4
+
+        static let invalidPulseRate: UInt8 = 0xFF
+        static let invalidSpo2: UInt8 = 0x7F
+        static let invalidPlethysmograph: UInt8 = 0x00
+    }
+
+    private enum ParamType {
+        case plethysmograph
+        case pulseRate
+        case spo2
+    }
 
     func parse(dataStream: Data) -> [HeartParams] {
         var dataPoints: [HeartParams] = []
@@ -44,16 +63,16 @@ struct BerryMedPulseOx: HeartParamsParsingProtocol {
     // the most sig bit of the zeroth byte is the package head and should be 1, the most significant bit of the next four bytes should be 0.
     // assumes bytes have already been checked for alignment (bit 7
     private func parse(dataPacket: Data) -> HeartParams? {
-        if dataPacket.count == berrymedDataPackSize, dataPacket[0] & 0x80 == 0x80 {
-            var plethysmograph: UInt8 = dataPacket[IndexOf.plethysmographBits]
-            var pulseRate: UInt8 = dataPacket[IndexOf.pulseRateBits0_6] | ((dataPacket[IndexOf.pulseRateBits7] & (0x01 << 6)) << 1)
-            var spo2: UInt8 = dataPacket[IndexOf.spo2Bits]
+        if dataPacket.count == Constants.berrymedDataPackSize, dataPacket[0].isBitSet(atIndex: Constants.bitIndexOfHeader) {
+            var plethysmograph: UInt8 = dataPacket[Constants.byteIndexOfPlethysmographBits]
+            var pulseRate: UInt8 = dataPacket[Constants.byteIndexOfPulseRateBits0_6] | (dataPacket[Constants.byteIndexOfPulseRateBits7].isolateBit(atIndex: 6)).shift(.right, by: 1)
+            var spo2: UInt8 = dataPacket[Constants.byteIndexOfSpo2Bits]
             plethysmograph = sanitize(value: plethysmograph, for: .plethysmograph)
             pulseRate = sanitize(value: pulseRate, for: .pulseRate)
             spo2 = sanitize(value: spo2, for: .spo2)
             return HeartParams(plethysmograph: plethysmograph, pulseRate: pulseRate, spo2: spo2)
         } else {
-            Log.info("Package data length is not \(berrymedDataPackSize) Bytes")
+            Log.info("Package data length is not \(Constants.berrymedDataPackSize) Bytes")
             return nil
         }
     }
@@ -62,48 +81,60 @@ struct BerryMedPulseOx: HeartParamsParsingProtocol {
         var headerByteIndices: [Int] = []
         // find header bytes ie bytes where the MSb is set
         for (i, byte) in dataStream.enumerated() {
-            if byte & 0x80 == 0x80 {
+            if byte.isBitSet(atIndex: Constants.bitIndexOfHeader) {
                 headerByteIndices.append(i)
             }
         }
 
-        // verify header bytes are 5 bytes apart. remove any that isn't 5 bytes away from the next
         var indicesOfIncompletePackets: Set<Int> = []
-        for i in 1...(headerByteIndices.count-1) {
-            if headerByteIndices[i] - headerByteIndices[i-1] != berrymedDataPackSize {
-                indicesOfIncompletePackets.insert(headerByteIndices[i-1])
-            }
-        }
 
-        if let last = headerByteIndices.last, (dataStream.count - last) != berrymedDataPackSize {
-            indicesOfIncompletePackets.insert(last)
+        if headerByteIndices.isEmpty {
+            return headerByteIndices
+        } else {
+            // verify header bytes are 5 bytes apart. remove any that isn't 5 bytes away from the next
+            for i in 1...(headerByteIndices.count-1) {
+                if (headerByteIndices[i] - headerByteIndices[i-1]) != Constants.berrymedDataPackSize {
+                    indicesOfIncompletePackets.insert(headerByteIndices[i-1])
+                }
+            }
+
+            if let last = headerByteIndices.last, (dataStream.count - last) != Constants.berrymedDataPackSize {
+                indicesOfIncompletePackets.insert(last)
+            }
         }
 
         return Set(headerByteIndices).subtracting(indicesOfIncompletePackets).sorted()
     }
 
-    private struct IndexOf {
-        static let plethysmographBits = 1
-        static let pulseRateBits7 = 2
-        static let pulseRateBits0_6 = 3
-        static let spo2Bits = 4
-    }
-
-    private enum ParamType {
-        case plethysmograph
-        case pulseRate
-        case spo2
-    }
-
     private func sanitize(value: UInt8, for type: ParamType) -> UInt8 {
-        let invalidPulseRate: UInt8 = 0xFF
-        let invalidSpo2: UInt8 = 0x7F
-        let invalidPlethysmograph: UInt8 = 0x00
-
         switch type {
-        case .plethysmograph: return value == invalidPlethysmograph ? 0 : value
-        case .spo2: return value == invalidSpo2 ? 0 : value
-        case .pulseRate: return value == invalidPulseRate ? 0 : value
+        case .plethysmograph: return value == Constants.invalidPlethysmograph ? 0 : value
+        case .spo2: return value == Constants.invalidSpo2 ? 0 : value
+        case .pulseRate: return value == Constants.invalidPulseRate ? 0 : value
         }
+    }
+}
+
+fileprivate extension UInt8 {
+    enum Direction {
+        case left
+        case right
+    }
+
+    func shift(_ direction: Direction, by places: UInt8) -> UInt8 {
+        switch direction {
+        case .left: return self >> places
+        case .right: return self << places
+        }
+    }
+
+    func isolateBit(atIndex index: UInt8) -> UInt8 {
+        let mask = UInt8(0x01).shift(.right, by: index)
+        return self & mask
+    }
+
+    func isBitSet(atIndex index: UInt8) -> Bool {
+        let mask = UInt8(0x01).shift(.right, by: index)
+        return self.isolateBit(atIndex: index) == mask
     }
 }
